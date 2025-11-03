f75a4b6501bf0b15b3cfd826d1819f9f0bfa51c2
Laurent Demailly <ldemailly@gmail.com>
2025-11-03 10:02:50 -0800

    Update main.go

--- main.go
+++ main.go
@@ -157,6 +157,8 @@ func (st *State) Tick() bool {
 		st.RepaintAll()
 	default:
 		// Regen a new maze on any other key
+	default:
+		// Regen a new maze on any other key
 		_ = st.ap.OnResize()
 	}
 	return true
